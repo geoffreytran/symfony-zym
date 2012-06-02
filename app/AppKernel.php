d6f16daa1e94016f405cf1c9c9e1eeca99c6a073
Geoffrey Tran <geoffrey.tran@rapp.com>
2012-06-02 11:57:19 -0500

    Updating symfony-zym

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,6 +1,6 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
+use Zym\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
@@ -19,13 +19,39 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            
+            // Symfony Extra
+            new JMS\SerializerBundle\JMSSerializerBundle($this),
+            
+            new FOS\UserBundle\FOSUserBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new OpenSky\Bundle\RuntimeConfigBundle\OpenSkyRuntimeConfigBundle(),
+            new Liip\ThemeBundle\LiipThemeBundle(),
+            
+            // Zym
+            new Zym\Bundle\FrameworkBundle\ZymFrameworkBundle(),
+            new Zym\Bundle\UserBundle\ZymUserBundle(),
+            new Zym\Bundle\FieldBundle\ZymFieldBundle(),
+            new Zym\Bundle\NodeBundle\ZymNodeBundle(),
+            new Zym\Bundle\RouterBundle\ZymRouterBundle(),
+            new Zym\Bundle\SecurityBundle\ZymSecurityBundle(),
+            new Zym\Bundle\TaxonomyBundle\ZymTaxonomyBundle(),
+            new Zym\Bundle\SessionBundle\ZymSessionBundle(),
+            new Zym\Bundle\MenuBundle\ZymMenuBundle(),
+            new Zym\Bundle\RuntimeConfigBundle\ZymRuntimeConfigBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), $this->debugEnvironments)) {
             $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            
+            $bundles[] = new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
--- web/app.php
+++ web/app.php
@@ -1,15 +1,38 @@
 <?php
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+// Environment
+$environment = isset($_SERVER['ENVIRONMENT']) ? $_SERVER['ENVIRONMENT']
+											  : 'prod';
+
+if ($environment == 'debug') {               
+	require_once __DIR__ . '/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+	require_once __DIR__ . '/../app/autoload.php';
+} else {        
+	require_once __DIR__ . '/../app/bootstrap.php.cache';
+}
+
+require_once __DIR__ . '/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
-$request = Request::createFromGlobals();
+// Whether to force debugging
+$debug       = isset($_SERVER['DEBUG'])       ? (bool) $_SERVER['DEBUG']      : null;
+
+// Wether to use the internal app cache
+$httpCache   = isset($_SERVER['HTTP_CACHE'])  ? (bool) $_SERVER['HTTP_CACHE'] : false;
+
+// Setup the kernel and handle the request
+$kernel = new AppKernel($environment, $debug);
+
+// Http Caching
+if ($httpCache) {
+	require_once __DIR__ . '/../app/AppCache.php';
+
+	$kernel = new AppCache($kernel);
+}
+
+// Handle the request
+$request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
