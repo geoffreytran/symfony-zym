8969f85972eb9f9f1fd4bced8465ee406a7cd331
Geoffrey Tran <geoffrey.tran@gmail.com>
2013-08-05 15:35:53 -0500

    Chef updates

--- data/chef/roles/web.rb
+++ data/chef/roles/web.rb
@@ -6,7 +6,7 @@ run_list(
   "role[default]",
 
   "recipe[java]",
-  "recipe[php54]",
+  "recipe[php]",
   "recipe[sqlite]",
   "recipe[nodejs]",
   "recipe[nodejs::npm]",
@@ -23,22 +23,36 @@ run_list(
   "recipe[apache2::mod_headers]",
   "recipe[apache2::mod_rewrite]",
   "recipe[apache2::mod_setenvif]",
-  "recipe[apache2::mod_xsendfile]",
+#  "recipe[apache2::mod_xsendfile]",
   "recipe[apache2::mod_ssl]"
 )
 
 # Attributes applied if the node doesn't have it set already.
 default_attributes({
-  :apache2 => {
-    :serversignature => 'off'
+  :apache => {
+    :default_site_enabled => false,
+    :serversignature      => "Off",
+    :traceenable          => "Off"
   },
 
+#  :php => {
+#    :ext_conf_dir => "/etc/php5/mods-available" # php54
+#  },
+
   :nodejs => {
-    :install_method => 'source',
-    :dir            => '/usr',
-    :npm            => '1.2.30'
+    :install_method => "source",
+    :dir            => "/usr",
+    :npm            => "1.2.30"
   },
 })
 
 # Attributes applied no matter what the node has set already.
-#override_attributes()
+#override_attributes(
+#  :apache => {
+#    # Removed authz_default because it doesn't exist in Apache 2.4
+#    :default_modules => %w{
+#      status alias auth_basic authn_file authz_groupfile authz_host authz_user autoindex
+#      dir env mime negotiation setenvif
+#    }
+#  }
+#)
