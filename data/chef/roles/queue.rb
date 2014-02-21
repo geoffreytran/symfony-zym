db894559e292098c5070afcea994b15408a498c6
Geoffrey Tran <geoffrey.tran@gmail.com>
2014-02-21 10:52:58 -0600

    Paginator CSS fix and Redis logging to notice

--- data/chef/roles/queue.rb
+++ data/chef/roles/queue.rb
@@ -3,16 +3,22 @@ description "Configures the node as a queue server."
 
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 run_list(
-  "recipe[redisio::install]",
-  "recipe[redisio::enable]"
+    "recipe[redisio::install]",
+    "recipe[redisio::enable]"
 )
 
 # Attributes applied if the node doesn't have it set already.
 default_attributes({
-  :beanstalkd => {
-    :daemon_opts       => "",
-    :start_during_boot => true
-  }
+    :beanstalkd => {
+        :daemon_opts       => "",
+        :start_during_boot => true
+    },
+
+    :redisio => {
+        :default_settings => {
+            :loglevel => "notice"
+        }
+    }
 })
 
 # Attributes applied no matter what the node has set already.
